1fbbc6603cb1d3bb99408f690b325d3993942adf
Oystres <128433737+Oystres@users.noreply.github.com>
2024-05-04 16:17:15 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,17 @@ import AdminPanel from './components/AdminPanel';
 import AdminLogin from './components/AdminLogin';
 import StudentLogin from './components/StudentLogin';
 import TeacherLogin from './components/TeacherLogin';
+import AdminAsessments from './components/AdminAsessments';
+import DeleteStudent from './components/DeleteStudent';
+import DeleteTeacher from './components/DeleteTeacher';
+import StreamForm from './components/StreamForm';
+import StudentAsessments from './components/StudentAsessments';
+import StudentDebts from './components/StudentDebts';
+import StudentGroup from './components/StudentGroup';
+import TeacherAsessment from './components/TeacherAsessment';
+import TeacherDebt from './components/TeacherDebt';
+import TeacherGroups from './components/TeacherGroups';
+import TeachersList from './components/TeachersList';
 
 function App() {
   return (
@@ -22,6 +33,17 @@ function App() {
             <Route path="/AdminLogin" element={<AdminLogin />} />
             <Route path="/StudentLogin" element={<StudentLogin />} />
             <Route path="/TeacherLogin" element={<TeacherLogin />} />
+            <Route path="/AdminAsessments" element={<AdminAsessments />} />
+            <Route path="/DeleteStudent" element={<DeleteStudent />} />
+            <Route path="/DeleteTeacher" element={<DeleteTeacher />} />
+            <Route path="/StreamForm" element={<StreamForm />} />
+            <Route path="/StudentAsessments" element={<StudentAsessments />} />
+            <Route path="/StudentDebts" element={<StudentDebts />} />
+            <Route path="/StudentGroup" element={<StudentGroup />} />
+            <Route path="/TeacherAsessment" element={<TeacherAsessment />} />
+            <Route path="/TeacherDebt" element={<TeacherDebt />} />
+            <Route path="/TeacherGroups" element={<TeacherGroups />} />
+            <Route path="/TeachersList" element={<TeachersList />} />
           </Routes>
         </Router>
     </div>
